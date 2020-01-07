9e73e897af520cef6a95b1325f2058c76428a1a2
eddyjunior <neggoh@gmail.com>
2020-01-07 19:29:58 -0200

    Adicionar arquivos de projeto.

---
+++ Palmeiras/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Palmeiras.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
